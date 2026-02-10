7fb3c3ae3d82f79fadec01affc6ab89214995940
Dominic_SE <105536723+SE-Dominic@users.noreply.github.com>
2026-02-10 01:12:11 +0000

    created files

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os, shutil
+#/workspaces/File-Organizer/dummy_folder/one.txt
 def organize_folder(folder_name):
     #loop through files in directory
     for filename in os.listdir(folder_name):
+        print(f"printing filename: {filename}")
         file_path = os.path.join(folder_name, filename) #/dummy_folder/image.png    
         #if its a directory skip it
         if os.path.isdir(file_path):
@@ -18,9 +20,23 @@ def organize_folder(folder_name):
         #move file
         shutil.move(file_path, os.path.join(new_folder, filename))
         print(f"Moved: {filename} --> {new_folder}")
+
+def rename_file(folder_name):
+    file_to_be_found = str(input("Enter file name to be found: "))
+    for file in os.listdir(folder_name):
+        if file == file_to_be_found:
+            newName = str(input("File was found! Rename to: ")) #ex. onee.txt
+            fileRename = f"{folder_name}/{newName}"
+            os.rename(file, fileRename)
+        else:
+            continue
+    
+
+
 if __name__ == "__main__":
-    downloads_file = "--file/path/here--"
+    downloads_file = "/workspaces/File-Organizer/dummy_folder"
     if not os.path.isdir(downloads_file):
         print("Invalid folder path. Try again.")
     else:
-        organize_folder(downloads_file)
+        rename_file(downloads_file)
+        #organize_folder(downloads_file)
